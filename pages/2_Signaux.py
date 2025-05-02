7a02590ab82d5bd57c4cbbc68e2327fbe70fb828
metisted19 <metisted19@gmail.com>
2025-05-02 19:05:10 +0200

    Ajout du module ta pour analyse technique

--- pages/2_Signaux.py
+++ pages/2_Signaux.py
@@ -165,8 +165,8 @@ if os.path.exists(fichier_data):
             # --- Actualités financières ---
             st.subheader("🗞️ Actualités financières récentes")
             try:
-               flux_rss = "https://www.investing.com/rss/news_301.rss"
-               flux = feedparser.parse(flux_rss)
+                flux_rss = "https://www.investing.com/rss/news_301.rss"
+                flux = feedparser.parse(flux_rss)
                 if flux.entries:
                     for entry in flux.entries[:5]:
                         st.markdown(f"🔹 [{entry.title}]({entry.link})", unsafe_allow_html=True)
