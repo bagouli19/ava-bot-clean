1d9a2ea7edca01f61fb1b622e064238c790bb06c
metisted19 <metisted19@gmail.com>
2025-06-28 11:34:12 +0200

    Re-added prompt_oblivia.txt without LFS

--- bert.py
+++
@@ -1,6 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-# Téléchargement du modèle BERT localement
-model = SentenceTransformer('bert-base-nli-mean-tokens')
-
-print("✅ Modèle BERT téléchargé et prêt à être utilisé.")
--- data/voir_donnees.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("data/donnees_aapl.csv")
-print(df.head())
-
- 
